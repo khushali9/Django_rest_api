d03fb32ed730b3e75ec4dae91dc36bac77e69924
Khushali Desai <khushali.desai@sjsu.edu>
2015-05-23 22:55:19 -0700

    done with api

--- rest_example/rest_example/urls.py
+++ rest_example/rest_example/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from restapp import views
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^users/', views.UserList.as_view()),
+    url(r'^user/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),)
